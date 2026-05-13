b5642e6f2e20c6775f88f347766fe37aa11bd148
Priyaharini12 <priyaharini1206@gmail.com>
2026-05-13 10:26:25 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -7,7 +7,6 @@ module tb ();
   initial begin
     $dumpfile("tb.fst");
     $dumpvars(0, tb);
-    #1;
   end
 
   // Inputs
@@ -35,14 +34,14 @@ module tb ();
       .VGND(VGND),
 `endif
 
-      .ui_in   (ui_in),
-      .uo_out  (uo_out),
-      .uio_in  (uio_in),
-      .uio_out (uio_out),
-      .uio_oe  (uio_oe),
-      .ena     (ena),
-      .clk     (clk),
-      .rst_n   (rst_n)
+      .ui_in(ui_in),
+      .uo_out(uo_out),
+      .uio_in(uio_in),
+      .uio_out(uio_out),
+      .uio_oe(uio_oe),
+      .ena(ena),
+      .clk(clk),
+      .rst_n(rst_n)
   );
 
 endmodule
