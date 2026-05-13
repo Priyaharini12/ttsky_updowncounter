8b68b7e9471a286abd1e8960445f07d54fdb4723
Priyaharini12 <priyaharini1206@gmail.com>
2026-05-13 10:31:31 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,32 +1,47 @@
+# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
+# SPDX-License-Identifier: Apache-2.0
+
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import RisingEdge
+from cocotb.triggers import RisingEdge, Timer
 
 
 @cocotb.test()
 async def test_project(dut):
 
+    dut._log.info("Starting Test")
+
+    # Start clock
     clock = Clock(dut.clk, 10, unit="ns")
     cocotb.start_soon(clock.start())
 
+    # Initialize
     dut.ena.value = 1
+    dut.ui_in.value = 0
+    dut.uio_in.value = 0
 
-    # reset
+    # Apply reset
     dut.rst_n.value = 0
 
     for _ in range(5):
         await RisingEdge(dut.clk)
 
+    # Release reset
     dut.rst_n.value = 1
 
-    # UP mode
+    # Set UP mode
     dut.ui_in.value = 1
 
-    # wait one cycle
+    # Wait for first active clock
     await RisingEdge(dut.clk)
 
-    # check counting
-    for expected in range(1, 6):
+    expected = 1
+
+    # Test UP counter
+    for _ in range(5):
+
+        # Wait tiny delay for <= update
+        await Timer(1, unit="ns")
 
         observed = dut.uo_out.value.to_unsigned() & 0xF
 
@@ -34,6 +49,11 @@ async def test_project(dut):
             f"Expected={expected} Observed={observed}"
         )
 
-        assert observed == expected
+        assert observed == expected, \
+            f"Expected {expected}, Got {observed}"
+
+        expected = (expected + 1) % 16
 
         await RisingEdge(dut.clk)
+
+    dut._log.info("TEST PASSED")
