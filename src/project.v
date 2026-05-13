60d8d736e25039ca16c9a6658d778a289a09f4aa
Priyaharini12 <priyaharini1206@gmail.com>
2026-05-13 09:53:31 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -6,47 +6,50 @@
 `default_nettype none
 
 module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path
-    input  wire       ena,      // always 1 when powered
-    input  wire       clk,      // clock
-    input  wire       rst_n     // active low reset
+    input  wire [7:0] ui_in,
+    output wire [7:0] uo_out,
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+    input  wire       ena,
+    input  wire       clk,
+    input  wire       rst_n
 );
 
-    // 4-bit counter register
+    // 4-bit counter
     reg [3:0] count;
 
-    // mode input
-    // ui_in[0] = 1 → up counter
-    // ui_in[0] = 0 → down counter
+    // mode select
+    // ui_in[0] = 1 → UP
+    // ui_in[0] = 0 → DOWN
     wire mode;
 
     assign mode = ui_in[0];
 
-    // Up/Down Counter
+    // ACTIVE LOW RESET
     always @(posedge clk or negedge rst_n)
     begin
         if (!rst_n)
             count <= 4'b0000;
+
         else begin
+
             if (mode)
                 count <= count + 1'b1;
+
             else
                 count <= count - 1'b1;
         end
     end
 
-    // Output counter value
+    // Output mapping
     assign uo_out = {4'b0000, count};
 
-    // Unused bidirectional IOs
+    // Unused IOs
     assign uio_out = 8'b00000000;
     assign uio_oe  = 8'b00000000;
 
-    // Prevent unused warnings
+    // Prevent warnings
     wire _unused = &{ena, uio_in, ui_in[7:1], 1'b0};
 
 endmodule
